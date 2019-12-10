2c0c3ce7511e0b300381cfe820b7411c7ec12b41
LuanLee <lecongluan111198@gmail.com>
2019-12-10 22:40:04 +0700

    update orm

---
+++ SEP_CRUD/Entity/ForeignKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEP_CRUD.Entity
+{
+    class ForeignKey
+    {
+        public string column { get; set; }
+        public KeyValuePair<string, string> reference { get; set; } //tablename
+    }
+}
---
+++ SEP_CRUD/Entity/PrimaryKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEP_CRUD.Entity
+{
+    class PrimaryKey
+    {
+        public List<string> columns { get; set; }
+        public Dictionary<string, string> references { get; set; } //tablename - columnname
+
+    }
+}

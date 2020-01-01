128d5077aec9e9341cadb9477d4525f946e5fbcb
Kim Ninh Huynh <kimninh1610@gmail.com>
2020-01-01 09:39:11 +0700

    add SQLHibernateTest project

---
+++ SQLHibernateTest/Firm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLHibernate.Define.HAttribute;
+using SQLHibernate.Define.SQLServer;
+
+namespace SQLHibernateTest
+{
+    [Table("Firm", "dbo")]
+    class Firm
+    {
+        private int? id;
+        private string name;
+        private bool delete;
+
+        [Field("IDFirm", FieldFlags.Key)]
+        public int? IDFirm
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        [Field("NameFirm")]
+        public string NameFirm
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        [Field("isDelete")]
+        public bool isDelete
+        {
+            get { return delete; }
+            set { delete = value; }
+        }
+    }
+}
